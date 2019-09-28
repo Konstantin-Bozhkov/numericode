6d6f68f503e222a77400932d08efdcadcd5e4879
Konstantin Bozhkov <konstantin.bozhkov@yahoo.com>
2019-09-28 19:54:46 +0100

    Decoder added using angular 2 way data binding and pipes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { DecoderPipe } from './decoder.pipe';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        FormsModule, RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        DecoderPipe
       ],
     }).compileComponents();
   }));
@@ -20,16 +23,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'numericode'`, () => {
+  it(`should have as title 'Welcome to Numericode'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('numericode');
+    expect(app.title).toEqual('Welcome to Numericode');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to numericode!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Numericode');
   });
 });
